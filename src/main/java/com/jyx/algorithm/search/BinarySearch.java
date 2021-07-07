cb32380c41d5bd07f47122e2aa0d56551ff7696a
jiangyuxin <972166944@qq.com>
2021-07-07 17:15:48 +0800

    first

--- src/main/java/com/jyx/algorithm/search/BinarySearch.java
+++ src/main/java/com/jyx/algorithm/search/BinarySearch.java
@@ -6,16 +6,25 @@ package com.jyx.algorithm.search;
  */
 public class BinarySearch {
 
+    /**
+     * 错误1.
+     * 错误2.
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
         int len = nums.length;
         if(len == 0)return -1;
         int i = 0;
         int j = len-1;
-        while (i != j){
+        //没有关注while条件，最开始是 i != j，可能出现数组元素1个，正好是目标的情况，就会bug
+        while (i <= j){
             int mid = (i+j)/2;
             if (nums[mid] == target)return mid;
-            if (nums[mid] < target)i = mid;
-            if (nums[mid] > target)j = mid;
+            //开始是i = mid，没有+1，-1之类的，mid都已经不等于了还等于mid干嘛
+            if (nums[mid] < target)i = mid+1;
+            if (nums[mid] > target)j = mid-1;
         }
         return -1;
     }
